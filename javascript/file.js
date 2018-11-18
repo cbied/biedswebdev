f5736fd2c6e1e2ed728bfb956afe8b21156fbaae
Christopher Biediger <cabiediger@gmail.com>
2018-11-18 15:10:52 +0800

    Add about me, skills, javaScript logic

--- javascript/file.js
+++ javascript/file.js
@@ -2,18 +2,39 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
 
+// mouse enter move right
 left.addEventListener("mouseenter", () => {
   container.classList.add("hover-left");
 });
-
+// mouse leave move back
 left.addEventListener("mouseleave", () => {
   container.classList.remove("hover-left");
 });
-
+// mouse enter move left
 right.addEventListener("mouseenter", () => {
   container.classList.add("hover-right");
 });
-
+// mouse enter move back
 right.addEventListener("mouseleave", () => {
   container.classList.remove("hover-right");
 });
+
+const leftBtn = document.querySelector("#leftBtn");
+const accordion = document.querySelector("#accordion");
+
+accordion.hidden = true;
+
+// toggle read more
+leftBtn.addEventListener("click", () => {
+  if (accordion.hidden === true) {
+    return accordion.hidden = false;
+  } else if (accordion.hidden === false) {
+    return accordion.hidden = true;
+  }
+})
+
+
+// accordion hidden when mouseleave
+accordion.addEventListener("mouseleave", () => {
+    accordion.hidden = true;
+})
